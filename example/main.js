1979cc76f2a18b805298bae7a41fc888318debfa
MakAndJo <makandjo@outlook.com>
2021-11-19 16:11:02 +0500

    update tests

--- example/main.js
+++ example/main.js
@@ -1,11 +1,12 @@
+const app_mount = document.getElementById('app-mount');
 function test_old() {
     class Clock extends GreenTree.AbstractElement {
         constructor (props) {
             super(props);
             this.state = {date: new Date().toUTCString()};
             setInterval(() => this.tick(), 1000);
-            console.log('Clock Children:', this.props.children)
-            console.log('Clock Cust:', this.props.cust)
+            //console.log('Clock Children:', this.props.children)
+            //console.log('Clock Cust:', this.props.cust)
         }
         tick () {
             this.setState({date: new Date().toUTCString()});
@@ -21,7 +22,7 @@ function test_old() {
         }
         create() {
             return (
-                GreenTree.createElement(Clock, { cust: 'green' }, GreenTree.createElement('span'), GreenTree.createElement('img'))
+                GreenTree.createElement(Clock, { cust: 'green', cp: this.props.customProperty }, GreenTree.createElement('span'), GreenTree.createElement('img'))
             )
         }
     }
@@ -47,15 +48,16 @@ function test_old() {
     const element = 
         GreenTree.createElement('root', { ref: ref_test }, // Ref test
             GreenTree.createElement('span', { class: ['first','second'], customProperty: 'lol' }, // Element with custom property
-                GreenTree.createElement(ClassElement), // Class Element
-                GreenTree.createElement(ClassElementWithProps, { prop: 1 }, ["Child1", "Child2"]), // Empty Class
-                ["Child1", "Child2"], // Array Element
+                "Basic TEXT", // RAW text
+                GreenTree.createElement(ClassElement, {customProperty: '5347'}), // Class Element
+                //GreenTree.createElement(ClassElementWithProps, { prop: 1 }, ["Child1", "Child2"]), // Empty Class
+                //["Child1", "Child2"], // Array Element
                 null, // Null Element
-                GreenTree.createElement('span', { unsafeHTML: true }, "<a>hello</a>"), // Raw HTML Element
-                GreenTree.createElement('svg', { ns: 'http://www.w3.org/2000/svg' }, // SVG Element
-                    GreenTree.createElement('circle', { ns: 'http://www.w3.org/2000/svg', cx: 50, cy: 50, r: 10, fill: "red"})
-                ),
-                var_element, // Variable Element
+                //GreenTree.createElement('span', { unsafeHTML: true }, "<a>hello</a>"), // Raw HTML Element
+                //GreenTree.createElement('svg', { ns: 'http://www.w3.org/2000/svg' }, // SVG Element
+                //    GreenTree.createElement('circle', { ns: 'http://www.w3.org/2000/svg', cx: 50, cy: 50, r: 10, fill: "red"})
+                //),
+                //var_element, // Variable Element
                 GreenTree.createElement(function_element), // Function Element
             )
         );
@@ -63,16 +65,32 @@ function test_old() {
     console.debug('Ref:', ref_test)
     console.debug('Element:', element)
 
-    GreenTree.Render(element, document.getElementById('app-mount')); 
+    GreenTree.Render(element, app_mount); 
 }
 
 function test_new() {
-    const element2 = GreenTree.createElement2('div', { class: 'one' },
+    class ClassElement extends GreenTree.Component {
+        constructor(props) {
+            super(props);
+        }
+        render() {
+            return (
+                GreenTree.createElement2('span', { class: "span-element"}, "text child")
+            )
+        }
+    }
+    function function_element(props) {
+        return GreenTree.createElement2('div', { class: "function-element" }, "function element");
+    }
+    var element2 = GreenTree.createElement2('root', { class: 'one' },
         GreenTree.createElement2('div', { class: 'two' }, 'text', 'textd'),
-        GreenTree.createElement2('div', { class: 'twosep' }, 'text2')
+        GreenTree.createElement2('div', { class: 'twosep' }, 'text2'),
+        GreenTree.createElement2(function_element),
+        GreenTree.createElement2(ClassElement),
     );
     console.debug('AltDom:', element2);
-    GreenTree.render(element2, document.getElementById('app-mount'));
+    GreenTree.render(element2, app_mount);
 }
 
+//test_old();
 test_new();
