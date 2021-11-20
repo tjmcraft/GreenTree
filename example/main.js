446e1425105cb6b399f33287513b430acc486d5a
MakAndJo <makandjo@outlook.com>
2021-11-20 10:24:39 +0500

    Update main.js

--- example/main.js
+++ example/main.js
@@ -1,12 +1,11 @@
 const app_mount = document.getElementById('app-mount');
 function test_old() {
+
     class Clock extends GreenTree.AbstractElement {
         constructor (props) {
             super(props);
             this.state = {date: new Date().toUTCString()};
             setInterval(() => this.tick(), 1000);
-            //console.log('Clock Children:', this.props.children)
-            //console.log('Clock Cust:', this.props.cust)
         }
         tick () {
             this.setState({date: new Date().toUTCString()});
@@ -69,24 +68,33 @@ function test_old() {
 }
 
 function test_new() {
-    class ClassElement extends GreenTree.AbstractElement {
-        constructor(props) {
+    class Clock extends GreenTree.AbstractElement {
+        constructor (props) {
             super(props);
+            this.state = {date: new Date().toUTCString()};
+            setInterval(() => this.tick(), 1000);
+        }
+        tick () {
+            this.setState({date: new Date().toUTCString()});
         }
         render() {
-            return (
-                GreenTree.createElement('span', { class: "span-element"}, "text child")
-            )
+            return GreenTree.createElement('div', { class: ['clock', 'sep'] }, `${this.state.date}`);
+        }
+    }
+    class ClassElement extends GreenTree.AbstractElement {
+        render() {
+            return GreenTree.createElement('span', { class: "span-element", cust: this.props.cust }, "text child");
         }
     }
     function function_element(props) {
-        return GreenTree.createElement('div', { class: "function-element" }, "function element");
+        return GreenTree.createElement('div', { class: "function-element", cust: props.cust }, "function element");
     }
     var element2 = GreenTree.createElement('root', { class: 'one' },
-        GreenTree.createElement('div', { class: 'two' }, 'text', 'textd'),
-        GreenTree.createElement('div', { class: 'twosep' }, 'text2'),
-        GreenTree.createElement(function_element),
-        GreenTree.createElement(ClassElement),
+        GreenTree.createElement('div', { class: 'two' }, 'text', 'text2'),
+        GreenTree.createElement('div', { class: 'twosep' }, 'text23'),
+        GreenTree.createElement(Clock),
+        GreenTree.createElement(function_element, { cust: 123 }),
+        GreenTree.createElement(ClassElement, { cust: 456 }),
     );
     console.debug('AltDom:', element2);
     GreenTree.Render(element2, app_mount);
