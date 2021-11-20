987fab0687bdbb5a1c343f9a7a802e045a941aa8
MakAndJo <makandjo@outlook.com>
2021-11-20 10:25:11 +0500

    Update Types.js

--- src/Types.js
+++ src/Types.js
@@ -1,3 +1,4 @@
 'use strict';
 export const GREEN_ELEMENT_TYPE = Symbol('green.element');
 export const GREEN_TREE_TYPE = Symbol('green.tree');
+export const RESERVED_PROPS = { children: true, ref: true, unsafeHTML: true, ns: true };
