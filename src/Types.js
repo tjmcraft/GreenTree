15f7cb044a4fcda5cfdfd08fc6614ac4ba939954
MakAndJo <makandjo@outlook.com>
2021-12-07 00:30:57 +0500

    update types

--- src/Types.js
+++ src/Types.js
@@ -2,3 +2,10 @@
 export const GREEN_ELEMENT_TYPE = Symbol('green.element');
 export const GREEN_TREE_TYPE = Symbol('green.tree');
 export const RESERVED_PROPS = { children: true, ref: true, unsafeHTML: true, ns: true };
+/**
+ * HTML nodeType values that represent the type of the node
+ */
+export const ELEMENT_NODE = 1;
+export const TEXT_NODE = 3;
+export const COMMENT_NODE = 8;
+export const DOCUMENT_NODE = 9;
