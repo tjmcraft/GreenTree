a5265acbb2626cbefc5187a2c77c2372b31f5152
MakAndJo <68428951+MakAndJo@users.noreply.github.com>
2022-02-22 23:22:15 +0500

    new tree

--- src/Tree/index.js
+++ src/Tree/index.js
@@ -1,171 +1,137 @@
-const { GREEN_ELEMENT_TYPE, RESERVED_PROPS, ELEMENT_NODE, GREEN_TREE_TYPE } = require("../Types");
-const { legacyCreateRootContainerFromDOM } = require("./LegacyRoot");
+import { DOCUMENT_NODE, ELEMENT_NODE, GREEN_ELEMENT_TYPE, HostComponent, LegacyRoot } from "../Types";
+import { createLeafRoot } from "./Leaf/Root";
 
-var emptyContextObject = {};
-{
-  Object.freeze(emptyContextObject);
+var now = () => new Date().getTime();
+var initialTime = now();
+var currentEventTime = 0;
+
+function isValidContainer(node) {
+    return !!(node && (node.nodeType == ELEMENT_NODE || node.nodeType == DOCUMENT_NODE));
 }
 
-function shouldConstruct$1(Component) {
-    var prototype = Component.prototype;
-    return !!(prototype && prototype.isGreenElement);
-}
-function isSimpleFunctionComponent(type) {
-    return typeof type === 'function' && !shouldConstruct$1(type) && type.defaultProps === undefined;
-}
-var hasOwnProperty$1 = Object.prototype.hasOwnProperty;
-
-function hasValidRef(config) {
-    {
-        if (hasOwnProperty$1.call(config, 'ref')) {
-            var getter = Object.getOwnPropertyDescriptor(config, 'ref').get;
-            if (getter && getter.isGreenWarning) {
-                return false;
-            }
-        }
+function getPublicRootInstance(container) {
+    var containerFiber = container.current;
+    
+    if (!containerFiber.child) {
+        return null;
     }
-    return config.ref !== undefined;
+
+    return containerFiber.child.stateNode;
 }
 
-const isValidProps = (props) => props != undefined && props != null && typeof props == "object";
-function setProps(element, props) {
-    if (!isValidProps(props)) {
-        console.warn("Invalid props:", props);
-        return !!0;
-    }
-    for (const prop in props) {
-        if (prop && props.hasOwnProperty(prop) && !RESERVED_PROPS.hasOwnProperty(prop)) {
-            let value = props[prop]
-            if (value instanceof Object) {
-                if (value instanceof Array) // if array
-                element.setAttribute(prop, value.filter(e => e).join(' '));
-                else if (typeof value === 'function' && value != null) // if function
-                element[prop] = value;
-                else Object.assign(element[prop], value);
-            } else {
-                if (value === true) // if simple true
-                element.setAttribute(prop, prop);
-                else if (typeof value === 'string' && value != null) // if string
-                element.setAttribute(prop, value);
-                else if (value !== false && value != null) // something else
-                element.setAttribute(prop, value.toString());
-            }
-        }
-    }
-    return !!1;
+var randomKey = Math.random().toString(36).slice(2);
+var internalInstanceKey = '__treeLeaf$' + randomKey;
+var internalPropsKey = '__treeProps$' + randomKey;
+var internalContainerInstanceKey = '__treeContainer$' + randomKey;
+var internalEventHandlersKey = '__treeEvents$' + randomKey;
+
+function precacheLeafNode(hostInst, node) {
+    node[internalInstanceKey] = hostInst;
 }
 
-function isMounted(component) {
-    return false;
+function markContainerAsRoot(hostRoot, node) {
+    node[internalContainerInstanceKey] = hostRoot;
 }
 
-var classComponentUpdater = {
-    isMounted: isMounted,
-    enqueueSetState: function (inst, payload, callback) {
-        console.debug('enqueueSetState:', {
-            instance: inst,
-            payload: payload,
-        });
-    },
-    enqueueReplaceState: function (inst, payload, callback) {
-        
-    },
-    enqueueForceUpdate: function (inst, callback) {
-       
-    }
-};
-
-function updateElement(element, container, parentComponent, callback) {
-    var current$1 = container.current;
-    var created = null;
-    if (typeof element === "object") {
-        if (element.$$typeof == GREEN_ELEMENT_TYPE) {
-            console.debug("element:", element);
-            if (typeof element.type === "string") {
-                const dom_element = document.createElement(element.type);
-                if (element.props) {
-                    //console.debug("Props:", element.props);
-                    setProps(dom_element, element.props); // Set properties to DOM element
-                    if (Array.isArray(element.props.children)) {
-                        const root_c = element.props.children.map(child => updateElement(child, null, element, null));
-                        for (const child of root_c) {
-                            if (child && child != null)
-                            if (!element.props.unsafeHTML) dom_element.append(child);
-                            else dom_element.innerHTML += child;
-                        }
-                    } else
-                    if (element.props.children && element.props.children != null)
-                    if (!element.props.unsafeHTML) dom_element.append(element.props.children);
-                    else dom_element.innerHTML += element.props.children;
-                }
-                console.debug("string comp:", dom_element);
-                element._gtrInternals = {
-                    type: element.type,
-                    stateNode: dom_element,
-                };
-                return dom_element;
-            } else if (typeof element.type === "function") {
-                if (isSimpleFunctionComponent(element.type)) {
-                    //console.warn("Simple Function Component:", element);
-                    const root_a = element.type.call(this, element.props);
-                    const c = updateElement(root_a, null, null);
-                    console.debug("function comp:", c);
-                    return c;
-                } else {
-                    //console.warn("Class Component:", element);
-                    var instance = constructClassInstance(element.type, element.props);
-                    var stateNode = instance.render();
-                    instance._gtrInternals = {
-                        type: element.type,
-                        stateNode: stateNode,
-                        return: instance,
-                    };
-                    const c = updateElement(stateNode, null);
-                    console.debug("cls comp:", c);
-                    return c;
-                }
-            }
-
-        } else {
-            console.debug("Invalid:", element)
-        }
-    } else if (typeof element === "string") {
-        return element;
-    }
-    //container.child = element;
+function unmarkContainerAsRoot(node) {
+    node[internalContainerInstanceKey] = null;
 }
 
-function legacyRender(parentComponent, children, container, callback) {
-    var root = container._greentreeRootContainer;
-    var superRoot;
-    if (!root) {
-        root = container._greentreeRootContainer = legacyCreateRootContainerFromDOM(container, false);
-        superRoot = root._internalRoot;
-        updateElement(children, superRoot, parentComponent, callback);
-    } else {
-        superRoot = root._internalRoot;
-        updateElement(children, superRoot, parentComponent, callback);
-    }
+function isContainerMarkedAsRoot(node) {
+    return !!node[internalContainerInstanceKey];
+}
+
+function createRootNode(container, tag, options) {
+    var root = createLeafRoot(container, tag);
+    markContainerAsRoot(root.current, container);
     return root;
 }
 
-function constructClassInstance(Component, props) {
-    var context = emptyContextObject;
-    if (Component.prototype && typeof Component.prototype.render === 'function') {
-        var instance = new Component(props, context);
-        instance.updater = classComponentUpdater;
-        return instance;
+function DOMBlockingRoot(container, tag, options) {
+    this._internalRoot = createRootNode(container, tag, options);
+}
+
+function createRoot(container, options) {
+    return new DOMBlockingRoot(container, LegacyRoot, options);
+}
+
+function createRootContainer(container) {
+    var rootSibling;
+    var warned = false;
+    while (rootSibling = container.lastChild) {
+        if (!warned && rootSibling.nodeType === DOCUMENT_NODE) {
+            console.warn('Root container is not empty!');
+            warned = true;
+        }
+        container.removeChild(rootSibling);
     }
+    return createRoot(container);
+}
+
+function requestEventTime() {
+    currentEventTime = now() - initialTime;
+    return currentEventTime;
+}
+
+function createUpdate(payload, eventTime) {
+    return Object.assign({
+        tag: "UpdateState",
+        eventTime: eventTime,
+        payload: payload,
+        callback: void 0,
+        next: null,
+    }, {});
+}
+
+function enqueueUpdate(node, update) {
+    var updateQueue = node.updateQueue;
+    if (updateQueue === null) {
+        console.warn("Already unmounted!", node);
+        return;
+    }
+    var sharedQueue = updateQueue.shared;
+    var pendingQueue = sharedQueue.pending;
+    sharedQueue.pending = update;
+    console.warn("enqeueUpdate:", updateQueue)
+}
+
+function scheduleUpdateOnLeaf(leaf, eventTime) {
+
+}
+
+function updateContainer(element, container, parentComponent, callback) {
+
+    const current = container.current;
+    const eventTime = requestEventTime();
+
+    const update = createUpdate({
+        element: element
+    }, eventTime);
+
+    enqueueUpdate(current, update);
+    scheduleUpdateOnLeaf(current, eventTime);
+    
+
+    console.debug("[updateContainer]", "{current}", current);
+    console.debug("[updateContainer]", "{element}", element);
+    console.debug("[updateContainer]", "{eventTime}", eventTime);
+}
+
+function legacyRender(parent, children, container, callback) {
+    var root = container._greentreeRootContainer;
+    var leafRoot;
+
+    if (!root) { // First time initial mount
+        root = container._greentreeRootContainer = createRootContainer(container);
+    }
+    leafRoot = root._internalRoot;
+    updateContainer(children, leafRoot, parent, callback);
+    return getPublicRootInstance(leafRoot);
 }
 
 function Render(element, container, callback) {
-    if (element.$$typeof !== GREEN_ELEMENT_TYPE) {
-        console.error("Element is not a GreenElement!");
-        return;
-    }
-    if (container.nodeType !== ELEMENT_NODE && !container.tagName) {
-        console.error("Container is not a node element!");
-        return;
+    if (!isValidContainer(container)) {
+        throw Error("Target container is not a DOM element.");
     }
     const root$1 = legacyRender(null, element, container, callback);
     //container.append(root$1);
@@ -173,4 +139,4 @@ function Render(element, container, callback) {
     return root$1;
 }
 
-exports.Render = Render;
+export { Render };
